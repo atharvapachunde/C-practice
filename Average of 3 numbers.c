2a84a5b89cbda565cba05072ff74b2ac14c5fa95
Atharva Pachunde <138585875+atharvapachunde@users.noreply.github.com>
2023-07-06 17:48:59 +0530

    Average of 3 numbers.c

---
+++ Average of 3 numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("enter a");
+    scanf("%d",&a);
+    printf("enter b");
+    scanf("%d",&b);
+    printf("enter c");
+    scanf("%d", &c);
+    
+    printf("Average of the 3 numbers is %d", (a+b+c)/2);
+    return 0;
+}
