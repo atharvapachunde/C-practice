d3278680664b166ed95beefe6fe1daa4c9a585b6
Atharva Pachunde <138585875+atharvapachunde@users.noreply.github.com>
2023-07-10 18:42:28 +0530

    Switch.c

---
+++ Switch.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+//switch
+int main(){
+    
+    int day;
+    printf("enter a number (1-7): ");
+    scanf("%d", &day);
+    
+    switch(day){
+        case 1: printf("monday");
+        break;
+        case 2: printf("tuesday");
+        break;
+        case 3:printf("wednesday");
+        break;
+        case 4:printf("thursday");
+        break;
+        case 5:printf("friday");
+        break;
+        case 6:printf("saturday");
+        break;
+        case 7:printf("sunday");
+        break;
+        default :printf("Not a valid day");
+    }
+}
