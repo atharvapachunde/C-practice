13f33d05408354f6ed5caa044663fb739ebb495d
Atharva Pachunde <138585875+atharvapachunde@users.noreply.github.com>
2023-07-20 12:39:16 +0530

    Char and float Loop.c

---
+++ Char and float Loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+        for(float f=1.0; f<=5.0; f++){
+        printf("%f\n", f);
+        }
+        printf("\n\n");
+        
+        for(char ch='a'; ch<='z'; ch++){
+            printf("%c\n", ch);
+        }
+        
+        return 0;
+}
