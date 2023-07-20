a3f441f3ba056b80b0b3d228c6f88f017872db55
Atharva Pachunde <138585875+atharvapachunde@users.noreply.github.com>
2023-07-20 12:43:19 +0530

    Do while.c

---
+++ Do while.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int i=1;
+    do{
+        printf("%d\n", i);
+        i++;
+    } while(i<=5);
+    printf("\n"); 
+    
+    int j=10;
+    do{
+        printf("%d\n", j);
+        j--;
+    } while(j>=0);
+    return 0;
+
+
+} 
