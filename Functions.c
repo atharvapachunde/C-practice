fd9d4fa142c38cb3fbc5367efe1a75f5d88cac3f
Atharva Pachunde <138585875+atharvapachunde@users.noreply.github.com>
2023-07-23 21:40:26 +0530

    Functions.c
    A small example of what a function is.

---
+++ Functions.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// declaration/prototype
+void printHello();
+
+int main(){
+   printHello();// function call
+}
+// function defintion
+void printHello(){
+    printf("Hello!\n");
+    printf("My name is Atharva");
+}
