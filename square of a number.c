95bca3e6f81d9cbc10ff395d85058264c56b60df
Atharva Pachunde <138585875+atharvapachunde@users.noreply.github.com>
2023-07-26 19:14:44 +0530

    square of a number.c

---
+++ square of a number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    
+    float n;
+    printf("Enter a Number: ");
+    scanf("%f", &n);
+    
+    printf("%f", pow(n, 2));
+    
+    return 0;
+}
